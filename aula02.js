7c54a3038e49e87571b8b849e5810b9fdcbc9403
vinipereira <vinicius.perira.1tds2.senai@gmail.com>
2024-03-12 15:42:51 -0300

    mudança no let mensagem

---
+++ aula02.js
@@ -0,0 +1,16 @@
+//aprendemos a iniciar/decalar variaveis.
+let aux = 30;
+let a = 10;
+let b = 20;
+let c = 20;
+let d = 15;
+
+// mostro a variavel para alguem/devs no console.
+//console.log(a);
+
+let soma= a + b;
+console.log (soma)
+
+ 
+let mensagem = " o resultado é:";
+console.log(mensagem + resulatado);
